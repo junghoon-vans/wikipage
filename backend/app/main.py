3af816c7426ee3567ac89fa1ba4e6de23d9db672
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-03 23:01:12 +0900

    chore: Setup cors configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import Depends
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from . import crud
@@ -7,6 +8,18 @@ from . import schemas
 from .database import SessionLocal
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db():
