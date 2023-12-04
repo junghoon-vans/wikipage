a008d62b0923f7a03bb502c7c3640003142d17e9
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-04 10:08:54 +0900

    refactor: Apply tailwind to Home.svelte

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,18 +1,48 @@
 <script>
-    import { onMount } from 'svelte';
+    import { onMount } from "svelte";
     let posts = [];
 
     async function fetchPosts() {
-    const res = await fetch('http://localhost:8000/posts/');
-    posts = await res.json();
+        const res = await fetch("http://localhost:8000/posts/");
+        posts = await res.json();
     }
 
     onMount(fetchPosts);
 </script>
 
-<div>
-    {#each posts as post (post.id)}
-    <h2>{post.title}</h2>
-    <p>{post.created_at}</p>
-    {/each}
+<div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                {#if posts.length > 0}
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Content</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            {#each posts as post (post.id)}
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm text-gray-900">
+                                            {post.title}
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm text-gray-500">
+                                            {post.content}
+                                        </div>
+                                    </td>
+                                </tr>
+                            {/each}
+                        </tbody>
+                    </table>
+                {:else}
+                    <div class="text-center text-gray-500">No posts found.</div>
+                {/if}
+            </div>
+        </div>
+    </div>
 </div>
