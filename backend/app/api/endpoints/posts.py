17c30ba40208eca3508cdb62bf98219535d0a1e8
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-07 01:03:10 +0900

    refactor: Improve project structure

--- backend/app/api/endpoints/posts.py
+++ backend/app/api/endpoints/posts.py
@@ -1,46 +1,34 @@
 from app import crud
 from app import schemas
-from app.database import SessionLocal
+from app.api.deps import get_db
 from fastapi import APIRouter
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
+
 router = APIRouter()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@router.get("/ping")
-async def ping():
-    return {"message": "pong"}
-
-
-@router.get("/posts/{post_id}")
+@router.get("{post_id}")
 def get_post(post_id: int, db: Session = Depends(get_db)):
     return crud.get_post(db, post_id)
 
 
-@router.get("/posts")
+@router.get("")
 def get_posts(db: Session = Depends(get_db)):
     return crud.get_posts(db)
 
 
-@router.post("/posts")
+@router.post("")
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return crud.create_post(db, post)
 
 
-@router.put("/posts/{post_id}")
+@router.put("{post_id}")
 def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     return crud.update_post(db, post_id, post)
 
 
-@router.delete("/posts/{post_id}")
+@router.delete("{post_id}")
 def delete_post(post_id: int, db: Session = Depends(get_db)):
     return crud.delete_post(db, post_id)
