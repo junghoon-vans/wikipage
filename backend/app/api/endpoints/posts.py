5051a32a299d7654b00fa4360fdf79cef2290098
Junghoon Ban <junghoon.ban@gmail.com>
2023-12-07 13:44:41 +0900

    fix: Invalid router path

--- backend/app/api/endpoints/posts.py
+++ backend/app/api/endpoints/posts.py
@@ -11,26 +11,26 @@ from sqlalchemy.orm import Session
 router = APIRouter()
 
 
-@router.get("{post_id}")
+@router.get("/{post_id}")
 def get_post(post_id: int, es: Elasticsearch = Depends(get_es)):
     return crud.get_post(es, post_id)
 
 
-@router.get("")
+@router.get("/")
 def get_posts(es: Elasticsearch = Depends(get_es)):
     return crud.get_posts(es)
 
 
-@router.post("")
+@router.post("/")
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return crud.create_post(db, post)
 
 
-@router.put("{post_id}")
+@router.put("/{post_id}")
 def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     return crud.update_post(db, post_id, post)
 
 
-@router.delete("{post_id}")
+@router.delete("/{post_id}")
 def delete_post(post_id: int, db: Session = Depends(get_db)):
     return crud.delete_post(db, post_id)
